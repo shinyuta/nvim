f0adbd2e4168fa47b41dab79d94026ec4ada48e4
Yuta Kirihara <shinyoota@gmail.com>
2024-05-21 23:15:40 -0500

    completions.lua file added for snippets
    completions.lua file added for snippets

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,45 +1,46 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
 
-    -- Deprecated as of 0.10 read --> https://github.com/neovim/neovim/pull/26193
-    -- vim.fn.sign_define("DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
-    -- vim.fn.sign_define("DiagnosticSignWarn", {text = " ", texthl = "DiagnosticSignWarn"})
-    -- vim.fn.sign_define("DiagnosticSignInfo", {text = " ", texthl = "DiagnosticSignInfo"})
-    -- vim.fn.sign_define("DiagnosticSignHint", {text = "󰌵", texthl = "DiagnosticSignHint"})
+		-- Deprecated as of 0.10 read --> https://github.com/neovim/neovim/pull/26193
+		-- vim.fn.sign_define("DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
+		-- vim.fn.sign_define("DiagnosticSignWarn", {text = " ", texthl = "DiagnosticSignWarn"})
+		-- vim.fn.sign_define("DiagnosticSignInfo", {text = " ", texthl = "DiagnosticSignInfo"})
+		-- vim.fn.sign_define("DiagnosticSignHint", {text = "󰌵", texthl = "DiagnosticSignHint"})
 
-    vim.diagnostic.config({
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = " ",
-          [vim.diagnostic.severity.WARN] = " ",
-          [vim.diagnostic.severity.INFO] = " ",
-          [vim.diagnostic.severity.HINT] = "󰌵",
-        },
-        linehl = {
-          [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-        },
-        numhl = {
-          [vim.diagnostic.severity.WARN] = "WarningMsg",
-          [vim.diagnostic.severity.INFO] = "InfoMsg",
-          [vim.diagnostic.severity.HINT] = "HintMsg",
-        },
-      },
-    })
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.INFO] = " ",
+					[vim.diagnostic.severity.HINT] = "󰌵",
+				},
+				linehl = {
+					[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+				},
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+					[vim.diagnostic.severity.WARN] = "WarningMsg",
+					[vim.diagnostic.severity.INFO] = "InfoMsg",
+					[vim.diagnostic.severity.HINT] = "HintMsg",
+				},
+			},
+		})
 
-    require("neo-tree").setup({
-      winbar = false,
-      statusline = true,
-      enable_diagnostics = true,
-      enable_git_status = true,
-    })
-  end,
+		require("neo-tree").setup({
+			winbar = false,
+			statusline = true,
+			enable_diagnostics = true,
+			enable_git_status = true,
+		})
+	end,
 }
