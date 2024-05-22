bfae363b9857222a604c2903bef3ba556b5dc2f8
Yuta Kirihara <shinyoota@gmail.com>
2024-05-22 00:09:21 -0500

    Added floaterm plugin to use with for Neovim terminal functionality and Lazygit
    Floaterm plugin for terminal and Lazygit

---
+++ lua/plugins/floatterm.lua
@@ -0,0 +1,7 @@
+return {
+  "voldikss/vim-floaterm",
+
+  config = function()
+    vim.keymap.set("n", "<C-m>", ":FloatermNew --height=0.8 --width=0.8 --titleposition=center --title=WinterFloat<CR>", {})
+  end,
+}
