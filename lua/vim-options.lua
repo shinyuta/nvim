1bbb3a5dd06ae9a8c2ae08f4c9fca968e88ba36a
shinyuta <shinyoota@gmail.com>
2024-05-24 10:47:42 -0500

    added tmux-navigator for linux config
    added tmux-navigator for linux config to use with tmux. Added some
    keybindings.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -25,3 +25,10 @@ vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")     -- move line up(n)
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")     -- move line down(n)
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
+
+-- neovim pane movement
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
